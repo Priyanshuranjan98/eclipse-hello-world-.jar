b2406da20aae4bd23d13b0e90d0833308a466009
prajwal <prajwal8120@gmail.com>
2022-11-01 19:24:36 +0530

    initial commit

---
+++ src/main/java/com/sonirk/hello_world_app/App.java
@@ -0,0 +1,13 @@
+package com.sonirk.hello_world_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
